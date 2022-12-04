48221da1225b63a99a7598a52b3812bc421e34e9
kalempster <mrpiter1337@gmail.com>
2022-12-04 01:50:53 +0100

    Wrong file names, whoops

--- day3/index2.ts
+++ day3/index2.ts
@@ -15,17 +15,24 @@ for (let index = "A"; index <= "Z"; index = String.fromCharCode(index.charCodeAt
 
 (async () => {
     const input = (await fs.readFile("input.txt")).toString().split("\r\n");
+    const teams: string[][] = [];
+
+    for (let index = 0; index < input.length; index++) {
+        if (index % 3 != 0) {
+            teams[Math.floor(index / 3)].push(input[index]);
+            continue;
+        }
+        teams.push([input[index]]);
+    }
 
     console.log(
-        input
-            .map((rucksack) => {
-                const firstComp = rucksack.slice(0, Math.floor(rucksack.length / 2)).split("");
-                const secondComp = rucksack.slice(Math.floor(rucksack.length / 2)).split("");
-                const [commonItem] = firstComp.filter((item) => secondComp.includes(item));
-                return commonItem;
-            })
-            .map((commonItem) => {
-                return prioForChars.find((o) => o.character == commonItem).priority;
+        teams
+            .map((team) => {
+                //Intersection to find badge
+                const [badge] = [team[0].split(""), team[1].split(""), team[2].split("")].reduce(
+                    (a, b) => a.filter((c) => b.includes(c))
+                );
+                return prioForChars.find((o) => o.character == badge).priority;
             })
             .reduce((a, b) => a + b)
     );
